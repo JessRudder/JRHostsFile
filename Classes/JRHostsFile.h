50dc0bddd96ca4d4e1ba3803c86c61f2b5096f38
JessRudder <thejessicarudder@gmail.com>
2015-10-25 18:48:27 -0400

    Add initWithFileName to allow initialization w/filename. Update init to call initWithFileName using default filename.

--- Classes/JRHostsFile.h
+++ Classes/JRHostsFile.h
@@ -1,5 +1,11 @@
 @interface JRHostsFile : NSObject
 
 -(id)init;
+-(id)initWithFileName:(NSString*)fileName;
+
+-(NSString*)defaultFileName;
+-(NSString*)getFileName;
+
+@property (nonatomic,strong) NSString* fileName;
 
 @end
--- Classes/JRHostsFile.m
+++ Classes/JRHostsFile.m
@@ -4,12 +4,28 @@
 
 - (id)init
 {
+    return [self initWithFileName: [self defaultFileName]];
+};
+
+- (id)initWithFileName:(NSString*)fileName
+{
     self = [super init];
+
     if (self)
     {
-        // superclass successfully initialized, further
-        // initialization happens here ...
+        self.fileName = fileName;
     }
     return self;
+};
+
+- (NSString*) defaultFileName
+{
+    return @"/etc/hosts";
+};
+
+- (NSString*) getFileName
+{
+    return self.fileName;
 }
+
 @end
